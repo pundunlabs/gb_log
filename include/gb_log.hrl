b71a8ba02c127447f844a16d5cb3e8183ed3f1a2
jonasf <jonas.falkevik@mobilearts.com>
2015-04-07 12:25:11 +0200

    Bug fix in gb_log macro
    Do not declare varible in macro.

--- include/gb_log.hrl
+++ include/gb_log.hrl
@@ -1,9 +1,8 @@
 -record(lf, {level, ts, mod, line, fmt, args}).
 -define(debug(FMT, Args), 
-	    Log = #lf{level=debug,
+	    gb_log:log(#lf{level=debug,
 		      mod=?MODULE,
 		      line=?LINE,
 		      fmt=FMT,
-		      args=Args},
-	    gb_log:log(Log)).
+		      args=Args}).
 
