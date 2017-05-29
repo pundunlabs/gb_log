58a5879a15aca01d1018f56ae9086e52fd4c1687
jonasf <jonas.falkevik@mobilearts.com>
2017-05-29 23:11:36 +0200

    Change copyright

--- c_src/daemonize.h
+++ c_src/daemonize.h
@@ -1,24 +1,20 @@
 /*
- _________________________________________________________________________
- Copyright (C) 2010 by
- MobileArts
- S-111 61 Stockholm
- Sweden
- Email:    info@mobilearts.com
- Homepage: http://www.mobilearts.com
- This file contains confidential information that remains the property
- of MobileArts, and may not be used, copied, circulated, or distributed 
- without prior written consent of MobileArts.
- _________________________________________________________________________
- %%%
- Revision:         '$Id$'
- Author:           'Jonas Falkevik'
- This version:     '$Revision$'
- Last modified:    '$Date$'
- Last modified by: '$Author$'
- _________________________________________________________________________
-   
- _________________________________________________________________________
+%%%===================================================================
+%% @author Jonas Falkevik
+%% @copyright 2017 Pundun Labs AB
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+%% implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%%===================================================================
 */
 
 #ifndef __daemonize__
